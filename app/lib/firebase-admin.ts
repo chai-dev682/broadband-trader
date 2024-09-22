f17034cc9f5f838d9f8a1d69d99c0a2982dafd94
zohaib-pro <zohaib.md07@gmail.com>
2024-09-23 03:11:38 +0500

    edited gitignore

--- app/lib/firebase-admin.ts
+++ app/lib/firebase-admin.ts
@@ -3,15 +3,18 @@ import admin from 'firebase-admin';
 import { readFileSync } from 'fs';
 import path from 'path';
 
-const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT || '';
+// const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT || '';
 
-// Load the service account key
+// // Load the service account key
+// const serviceAccount = JSON.parse(
+//   readFileSync(path.resolve(serviceAccountPath), 'utf8')
+// );
 const serviceAccount = JSON.parse(
-  readFileSync(path.resolve(serviceAccountPath), 'utf8')
+  process.env.FIREBASE_SERVICE_ACCOUNT_KEY || '{}'
 );
-
 if (!admin.apps.length) {
   admin.initializeApp({
+    //credential: admin.credential.cert(serviceAccount),
     credential: admin.credential.cert(serviceAccount),
     databaseURL: 'https://p2p-isp-contract-trading-default-rtdb.firebaseio.com' // Your database URL
   });
