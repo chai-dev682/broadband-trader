38313ee1e109cd13cc3f96935ea7f7d017984533
zohaib-pro <zohaib.md07@gmail.com>
2024-10-07 11:55:15 +0500

    started api

--- app/api/notes/create/route.ts
+++ app/api/notes/create/route.ts
@@ -1,26 +1,34 @@
 import admin from '@/app/lib/firebase-admin';
-import bcrypt from 'bcrypt';
+import { Collections } from '@/constants/firebase';
 
 export async function POST(req: Request) {
-  const { email, password, firstname, lastname, phone } = await req.json();
-
+  // todo: handle user authentication
   try {
-    const userRecord = await admin.auth().createUser({
-      email,
-      password
-    });
-
-    const hashedPassword = await bcrypt.hash(password, process.env.SALT || 10);
-
+    const {
+      contract_id,
+      sell_date,
+      internet_speed,
+      network_bandwidth_from_isp,
+      country,
+      asking_price,
+      sku,
+      order_number,
+      order_date
+    } = await req.json();
     const db = admin.firestore();
-    await db.collection('users').doc(userRecord.uid).set({
-      firstname,
-      lastname,
-      phone,
-      email,
-      password: hashedPassword,
-      createdAt: admin.firestore.FieldValue.serverTimestamp()
+    await db.collection(Collections.NOTES).doc().create({
+      contract_id,
+      sell_date
     });
+
+    // .set({
+    //   firstname,
+    //   lastname,
+    //   phone,
+    //   email,
+    //   password: hashedPassword,
+    //   createdAt: admin.firestore.FieldValue.serverTimestamp()
+    // });
 
     return new Response(JSON.stringify({ uid: userRecord.uid }), {
       status: 201
