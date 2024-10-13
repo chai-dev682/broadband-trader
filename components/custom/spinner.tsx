91d46611800e28d7554d6c103d7ebf8febfbc292
zohaib-pro <zohaib.md07@gmail.com>
2024-10-13 20:29:55 +0500

    fixed and improved spinner

--- components/custom/spinner.tsx
+++ components/custom/spinner.tsx
@@ -1,17 +1,36 @@
 'use client';
+
 import React from 'react';
 
-const DefaultSpinner = ({ size = 8, width = 4, color = 'primary' }) => {
-  const sizeCss = `h-${size} w-${size}`;
+interface DefaultSpinnerProps {
+  size?: 'xs' | 'sm' | 'md' | 'lg';
+  color?: string;
+  margin?: number;
+}
+
+const sizeMap = {
+  xs: 'w-4 h-4 border-2',
+  sm: 'w-8 h-8 border-2',
+  md: 'w-16 h-16 border-4',
+  lg: 'w-24 h-24 border-8'
+};
+
+const DefaultSpinner: React.FC<DefaultSpinnerProps> = ({
+  size = 'sm',
+  color = 'primary',
+  margin = 1
+}) => {
+  if (!color.includes('-')) {
+    // if color isn't passed as tailwind color like red-500, blue-500
+    // then convert the passed color to the tailwind color
+    color = `${color}-500`; //500 means medium shade
+  }
+
+  const sizeClasses = sizeMap[size];
   return (
-    <div
-      className={`flex items-center justify-center ${sizeCss} animate-spin`}
-      style={{ borderTopColor: 'transparent' }}
-    >
-      <div
-        className={`border-${width} rounded-full border-solid border-${color} border-t-transparent ${sizeCss}`}
-      ></div>
-    </div>
+    <span
+      className={`inline-block m-${margin} ${sizeClasses} border-${color} text-red border-t-${color} animate-spin rounded-full border-t-transparent`}
+    ></span>
   );
 };
 
